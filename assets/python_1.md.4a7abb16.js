555bd9695d7c0653564da3e3f6e7096b96be4b65
ychda <ychda@users.noreply.github.com>
2023-03-27 09:57:36 +0000

    deploy: 50f6f5351dad4e92dae246c3af001b6f2ceca516

---
+++ assets/python_1.md.4a7abb16.js
@@ -0,0 +1,16 @@
+import{_ as n,c as a,o,b as e,d as t}from"./app.b8c921aa.js";const v='{"title":"\u5E38\u89C1\u7684\u4E00\u4E9B\u95EE\u9898","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u5165\u95E8\u4EE3\u7801","slug":"\u5165\u95E8\u4EE3\u7801"}],"relativePath":"python/1.md","lastUpdated":1679910945000}',r={},s=e("h1",{id:"\u5E38\u89C1\u7684\u4E00\u4E9B\u95EE\u9898",tabindex:"-1"},[t("\u5E38\u89C1\u7684\u4E00\u4E9B\u95EE\u9898 "),e("a",{class:"header-anchor",href:"#\u5E38\u89C1\u7684\u4E00\u4E9B\u95EE\u9898","aria-hidden":"true"},"#")],-1),d=e("h2",{id:"\u5165\u95E8\u4EE3\u7801",tabindex:"-1"},[t("\u5165\u95E8\u4EE3\u7801 "),e("a",{class:"header-anchor",href:"#\u5165\u95E8\u4EE3\u7801","aria-hidden":"true"},"#")],-1),c=e("pre",null,[e("code",null,`\`\`\`python
+from turtle import *
+# \u8FD9\u91CC\u6DFB\u52A0\u4EE3\u7801
+done()
+\`\`\`
+
+\u6216\u8005
+
+\`\`\`python
+import turtle
+# \u8FD9\u91CC\u6DFB\u52A0\u4EE3\u7801
+turtle.done()
+\`\`\`
+
+\u5982\u679C\u4E0D\u6DFB\u52A0\u6700\u540E\u4E00\u884C\uFF0C\u7A0B\u5E8F\u8FD0\u884C\u7ED3\u675F\u540E\u7A97\u53E3\u5C31\u81EA\u52A8\u5173\u95ED\u3002
+`)],-1),i=[s,d,c];function l(h,_,p,u,f,m){return o(),a("div",null,i)}var $=n(r,[["render",l]]);export{v as __pageData,$ as default};
