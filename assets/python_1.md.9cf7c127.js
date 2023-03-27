f3c5eced2075fb65bcaa5541cdac00069c8cafec
ychda <ychda@users.noreply.github.com>
2023-03-27 09:54:58 +0000

    deploy: 1ab95393b1c4e5a9c489ffe4733c1ac506f04433

--- assets/python_1.md.9cf7c127.js
+++
@@ -1,5 +0,0 @@
-import{_ as e,c as a,o as t,a as r}from"./app.b8c921aa.js";const u='{"title":"\u5E38\u89C1\u7684\u4E00\u4E9B\u95EE\u9898","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u5165\u95E8\u4EE3\u7801","slug":"\u5165\u95E8\u4EE3\u7801"}],"relativePath":"python/1.md","lastUpdated":1679910406000}',o={},d=r(`<h1 id="\u5E38\u89C1\u7684\u4E00\u4E9B\u95EE\u9898" tabindex="-1">\u5E38\u89C1\u7684\u4E00\u4E9B\u95EE\u9898 <a class="header-anchor" href="#\u5E38\u89C1\u7684\u4E00\u4E9B\u95EE\u9898" aria-hidden="true">#</a></h1><h2 id="\u5165\u95E8\u4EE3\u7801" tabindex="-1">\u5165\u95E8\u4EE3\u7801 <a class="header-anchor" href="#\u5165\u95E8\u4EE3\u7801" aria-hidden="true">#</a></h2><pre><code>\`\`\`python
-from turtle import *
-# \u8FD9\u91CC\u6DFB\u52A0\u4EE3\u7801
-done()
-</code></pre><div class="language-"><pre><code></code></pre></div>`,4),n=[d];function _(c,s,i,h,l,p){return t(),a("div",null,n)}var m=e(o,[["render",_]]);export{u as __pageData,m as default};
