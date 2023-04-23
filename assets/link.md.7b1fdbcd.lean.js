b7e72c03ff93240033fc4a876eb34cc2db6263ff
ychda <ychda@users.noreply.github.com>
2023-04-23 23:01:42 +0000

    deploy: 780331d0ccc9463f97007ec365636a97b7e66943

---
+++ assets/link.md.7b1fdbcd.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as r,o as t,a as o}from"./app.b8c921aa.js";const b='{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"link.md","lastUpdated":1682290776000}',a={},n=o("",1),l=[n];function i(s,h,p,c,g,f){return t(),r("div",null,l)}var d=e(a,[["render",i]]);export{b as __pageData,d as default};
